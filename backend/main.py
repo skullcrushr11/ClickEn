08b6468bf4d1bf0e0dd5c81b478ed30dc2e5ecd4
manasg1111 <118659036+manasg1111@users.noreply.github.com>
2025-03-15 14:16:13 +0530

    feat: added db connection

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,23 @@
 from flask import Flask, jsonify
+from db.db import connect_to_mongo
+import json
 
 app = Flask(__name__)
+
+with app.app_context():
+    str_res = connect_to_mongo()
+    res = json.loads(str_res)
+    
+    if res["status"]  == 500:
+        print(res["error"])
+        exit(1)
+    print("Connected to MongoDB")
+    
 
 @app.route('/')
 def hello_world():
     response = {"message": "Hello, Flask!", "status": "success"}
-    return jsonify(response)  # Converts dictionary to JSON
+    return jsonify(response) 
 
 if __name__ == "__main__":
     app.run(debug=True)
